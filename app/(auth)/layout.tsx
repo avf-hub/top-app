e7f0729fe60809594be7b620e34e9ed692704a44
Andrey Falkovskiy <avf-91@yandex.ru>
2026-01-05 01:17:06 +0300

    Разделение layout и добавление групп роутов

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,31 @@
+import {Metadata} from "next";
+import {Noto_Sans_KR} from "next/font/google";
+
+export async function generateMetadata(): Promise<Metadata> {
+    return {
+        // ... Получение данных с backend
+        title: "MyTop - Наш лучший топ",
+        description: "MyTop by create next app"
+    };
+}
+
+const notoSansKR = Noto_Sans_KR({
+    subsets: ["cyrillic"],
+    weight: ["300", "400", "500", "700"],
+    display: "swap"
+});
+
+export default function RootLayout({
+                                       children,
+                                   }: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="ru">
+            <body className={notoSansKR.className}>
+            <div>Авторизация</div>
+            {children}
+            </body>
+        </html>
+    );
+}
---
+++ app/(site)/news/page.tsx
@@ -0,0 +1,13 @@
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: "News"
+};
+
+export default function News() {
+    return (
+        <div>
+            News
+        </div>
+    );
+}
