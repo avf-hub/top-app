60d958785875fbf8bb2cc74787e5c9ed72093be0
Andrey Falkovskiy <avf-91@yandex.ru>
2026-01-04 19:58:02 +0300

    Добавление компонента рейтинга - 2

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -1,4 +1,4 @@
-import {JSX, useEffect, useState} from "react";
+import {JSX, useEffect, useState, KeyboardEvent} from "react";
 import {RatingProps} from "@/components/Rating/Rating.props";
 import styles from "./Rating.module.css";
 import cn from "classnames";
@@ -13,13 +13,44 @@ export const Rating = ({isEditable = false, rating, setRating, ...props}: Rating
 
     const constractRating = (currentRating: number) => {
         const upgradeRating = ratingArray.map((r: JSX.Element, i: number) => {
-            return (<StarIcon
-                className={cn(styles.star, {
-                    [styles.filled]: i < currentRating
-                })}
-            />);
+            return (
+                <span className={cn(styles.star, {
+                            [styles.filled]: i < currentRating,
+                            [styles.editable]: isEditable
+                        })}
+                      onMouseEnter={() => changeDisplay(i + 1)}
+                      onMouseLeave={() => changeDisplay(rating)}
+                      onClick={() => onClick(i + 1)}
+                >
+                    <StarIcon
+                        tabIndex={isEditable ? 0 : -1}
+                        onKeyDown={(e: KeyboardEvent<SVGElement>) => isEditable && handleSpace(i + 1, e)}
+                    />
+                </span>
+            );
         });
         setRatingArray(upgradeRating);
+    };
+
+    const changeDisplay = (i: number) => {
+        if (!isEditable) {
+            return;
+        }
+        constractRating(i);
+    };
+
+    const onClick = (i: number) => {
+        if (!isEditable || !setRating) {
+            return;
+        }
+        setRating(i);
+    };
+
+    const handleSpace = (i: number, e: KeyboardEvent<SVGElement>) => {
+        if (e.code != "Space" || !setRating) {
+            return;
+        }
+        setRating(i);
     };
 
     return (<div {...props}>
