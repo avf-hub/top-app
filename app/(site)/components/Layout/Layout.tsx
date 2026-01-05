4e29e89f4620e485de92a5dd0c27b8f9d5a366ff
Andrey Falkovskiy <avf-91@yandex.ru>
2026-01-05 21:17:11 +0300

    Добавление HOC withLayout

--- app/(site)/components/Layout/Layout.tsx
+++ app/(site)/components/Layout/Layout.tsx
@@ -1,10 +1,10 @@
 import {LayoutProps} from "@/app/(site)/components/Layout/Layout.props";
-import {JSX} from "react";
+import {FunctionComponent, JSX} from "react";
 import {Footer} from "@/app/(site)/components/Footer/Footer";
 import {Sidebar} from "@/app/(site)/components/Sidebar/Sidebar";
 import {Header} from "@/app/(site)/components/Header/Header";
 
-export const Layout = ({children}: LayoutProps): JSX.Element => {
+const Layout = ({children}: LayoutProps): JSX.Element => {
     return (
         <>
             <Header/>
@@ -18,3 +18,13 @@ export const Layout = ({children}: LayoutProps): JSX.Element => {
         </>
     );
 }
+
+export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
+    return function withLayoutComponent(props: T): JSX.Element {
+        return (
+            <Layout>
+                <Component {...props} />
+            </Layout>
+        );
+    };
+};
