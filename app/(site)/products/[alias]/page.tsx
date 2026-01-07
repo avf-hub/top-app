2a52de0161ac0bc09ad96adabec94ac803578491
Andrey Falkovskiy <avf-91@yandex.ru>
2026-01-07 18:06:48 +0300

    generateStaticParams для получения алясов

--- app/(site)/products/[alias]/page.tsx
+++ app/(site)/products/[alias]/page.tsx
@@ -1,15 +1,22 @@
 import {Metadata} from "next";
 import React, {JSX} from "react";
 import {getPage} from "@/api/page";
+import {getMenu} from "@/api/menu";
 import {notFound} from "next/dist/client/components/not-found";
+import {MenuItem} from "@/interfaces/menu.interface";
 
 export const metadata: Metadata = {
     title: "Страница"
 };
 
+export async function generateStaticParams() {
+    const menu: MenuItem[] = await getMenu(0);
+    return menu.flatMap(item => item.pages.map(page => ({alias: page.alias})));
+}
+
 export default async function PageProducts({params}: {params: {alias: string}}): Promise<JSX.Element> {
-    const objParams = await params;
-    const alias = await getPage(objParams.alias);
+    const resolvedParams: {alias: string} = await params;
+    const alias = await getPage(resolvedParams.alias);
     if (!alias) {
         notFound();
     }
