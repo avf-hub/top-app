33f921644b12734c97228914b4718d75784f4cbb
Andrey Falkovskiy <avf-91@yandex.ru>
2026-01-08 17:59:08 +0300

    Добавление ревалидации страниц

--- api/menu.tsx
+++ api/menu.tsx
@@ -5,7 +5,8 @@ export async function getMenu(firstCategory: number): Promise<MenuItem[]> {
     const res: Response = await fetch(API.topPage.find, {
         method: "POST",
         body: JSON.stringify({firstCategory}),
-        headers: new Headers({"content-type": "application/json"})
+        headers: new Headers({"content-type": "application/json"}),
+        next: {revalidate: 10}
     });
     return res.json();
 }
