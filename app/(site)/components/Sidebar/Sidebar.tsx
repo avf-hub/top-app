d7c91da69a9c869cbddac10f3853b2d83d5639cb
Andrey Falkovskiy <avf-91@yandex.ru>
2026-01-12 13:57:38 +0300

    Верстка Sidebar

--- app/(site)/components/Sidebar/Sidebar.tsx
+++ app/(site)/components/Sidebar/Sidebar.tsx
@@ -1,10 +1,15 @@
 import {JSX} from "react";
 import {SidebarProps} from "@/app/(site)/components/Sidebar/Sidebar.props";
 import {Menu} from "@/app/(site)/components/Menu/Menu";
+import Logo from "@/app/logo.svg";
+import styles from "./Sidebar.module.css";
+import cn from "classnames";
 
-export const Sidebar = ({...props}: SidebarProps): JSX.Element => {
+export const Sidebar = ({className, ...props}: SidebarProps): JSX.Element => {
     return (
-        <div {...props}>
+        <div className={cn(className, styles.sidebar, styles.logo)} {...props}>
+            <Logo/>
+            <div>Поиск</div>
             <Menu/>
         </div>
     );
