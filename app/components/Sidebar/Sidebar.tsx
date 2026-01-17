2a91f32ed7185913aec604bd788ebb3fb1c27ad0
Andrey Falkovskiy <avf-91@yandex.ru>
2026-01-17 19:23:54 +0300

    Добавление Search компонента

--- app/components/Sidebar/Sidebar.tsx
+++ app/components/Sidebar/Sidebar.tsx
@@ -4,12 +4,13 @@ import {Menu} from "@/app/components/Menu/Menu";
 import Logo from "@/app/logo.svg";
 import styles from "./Sidebar.module.css";
 import cn from "classnames";
+import {Search} from "@/components";
 
 export const Sidebar = ({className, ...props}: SidebarProps): JSX.Element => {
     return (
         <div className={cn(className, styles.sidebar, styles.logo)} {...props}>
             <Logo/>
-            <div>Поиск</div>
+            <Search/>
             <Menu/>
         </div>
     );
---
+++ components/Search/Search.tsx
@@ -0,0 +1,42 @@
+"use client";
+import {ChangeEvent, JSX, useState, KeyboardEvent} from "react";
+import styles from "./Search.module.css";
+import cn from "classnames";
+import {SearchProps} from "@/components/Search/Search.props";
+import {Button, Input} from "@/components";
+import GlassIcon from "./glass.svg";
+import {useRouter} from "next/navigation";
+
+export const Search = ({className, ...props}: SearchProps): JSX.Element => {
+    const [search, setSearch] = useState<string>("");
+    const router = useRouter();
+
+    const goToSearch = () => {
+        router.push(`/search?q=${search}`);
+    };
+
+    const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === "Enter") {
+            goToSearch();
+        }
+    };
+
+    return (
+        <div className={cn(className, styles.search)} {...props}>
+            <Input
+                className={styles.input}
+                placeholder="Поиск..."
+                value={search}
+                onChange={(event: ChangeEvent<HTMLInputElement>) => setSearch(event.target.value)}
+                onKeyDown={handleKeyDown}
+            />
+            <Button
+                appearance="primary"
+                className={styles.button}
+                onClick={goToSearch}
+            >
+                <GlassIcon/>
+            </Button>
+        </div>
+    );
+};
