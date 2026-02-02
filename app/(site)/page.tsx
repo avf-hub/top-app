c3e496ed304c7290165666cbffb49a999a372a72
FalkovskiyAV <avf-91@yandex.ru>
2026-02-02 22:57:17 +0300

    App Router - Расчёт meta

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,16 +1,18 @@
-import {JSX} from "react";
-import {Metadata} from "next";
-import {Input, Textarea} from "@/components";
+import { JSX } from "react";
+import { Metadata } from "next";
+import { Input, Textarea } from "@/components";
+import { getPage } from "@/api/page";
 
-export const metadata: Metadata = {
-    title: "Главная страница"
+export async function generateMetadata({ params }: { params: { alias: string } }): Promise<Metadata> {
+    const page = await getPage(params.alias);
+    return { title: page?.metaTitle };
 };
 
 export default function Home(): JSX.Element {
     return (
         <>
-            <Input placeholder="тест"/>
-            <Textarea placeholder="тест area"/>
+            <Input placeholder="тест" />
+            <Textarea placeholder="тест area" />
         </>
     );
 }
